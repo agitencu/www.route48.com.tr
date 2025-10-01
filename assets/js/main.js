adfd5837b314fb5efd45578a76f5aaa725ff346a
Agit ENCU <agitencu@posta.mu.edu.tr>
2025-10-01 13:18:18 +0300

    project update.

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,10 +167,30 @@
 
 })();
 
-window.onload = function() {
+// Popup code
+  window.onload = function() {
     document.getElementById("popup").style.display = "flex";
   }
   function closePopup() {
     document.getElementById("popup").style.display = "none";
+  }
+
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle("active", i === index);
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
   }
 
